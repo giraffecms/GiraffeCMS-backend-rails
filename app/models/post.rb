83d7a755a739df20a03c8419204c8198c4586fd3
kkm <kubasurdej@gmail.com>
2019-02-04 21:18:07 +0100

    Add comment model, make spec for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+	has_many :comments, dependent: :destroy
+	validates :title, presence: true, length: { minimum: 2, maximum: 100 }
+	validates :body, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+	it { should validate_presence_of(:title) }
+	it { should validate_presence_of(:body) }
+
+	it { should validate_length_of(:title).is_at_most(100) }
+
+	it { should have_many(:comments).dependent(:destroy) }
 end
