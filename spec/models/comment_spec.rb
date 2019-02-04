49b70cb508531360f79c49b8901c43ebc5cf079c
kkm <kubasurdej@gmail.com>
2019-02-04 21:34:22 +0100

    Comment type and specs for it

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it { should validate_presence_of(:title) }
+	it { should validate_presence_of(:body) }
+
+	it { should validate_length_of(:title).is_at_most(56).is_at_least(3) }
+	it { should validate_length_of(:body).is_at_most(1000).is_at_least(3) }
+			
+	it { should belong_to(:post) }
 end
