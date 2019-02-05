1648f5dd6311de2d9c1f92d9ca77f3f75dfb8893
kkm <kubasurdej@gmail.com>
2019-02-05 13:00:50 +0100

    Working post create mutation

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,7 +4,7 @@ module Types
 	  field :id, ID, null: false
 	  field :title, String, null: false
 	  field :body, String, null: false
-	  field :comments, [CommentType], null: false
+	  field :comments, [CommentType], null: true
 	  field :created_at, GraphQL::Types::ISO8601DateTime, null: false
 	  field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 	end
