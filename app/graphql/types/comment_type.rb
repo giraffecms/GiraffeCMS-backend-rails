47f852bc08b7d3773ddf72e3eb87ae9c07009c42
kkm <kubasurdej@gmail.com>
2019-04-02 00:58:56 +0200

    Database seed for development

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -2,7 +2,7 @@ module Types
 	class CommentType < Types::BaseObject
 		description "Comment to a blog post"
 	  field :id, ID, null: false
-	  field :post, [PostType], null: false
+	  field :post, PostType, null: false
 	  field :title, String, null: false
 	  field :body, String, null: false
 	  field :created_at, GraphQL::Types::ISO8601DateTime, null: false
