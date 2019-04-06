ef04ef1355374ae38382b39b8468d6f51ad60004
kkm <kubasurdej@gmail.com>
2019-04-06 23:49:14 +0200

    Fix errors in user model and tests, user factory

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
   LETTERS_DASHES_DIGITS_REGEX = /\A[a-z0-9\-_]+\z/i
- 	validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 72 }, format: { with: LETTERS_DASHES_DIGITS_REGEX }
+ 	validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 72 }, format: { with: LETTERS_DASHES_DIGITS_REGEX }
  	validates :password, presence: true, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe User, type: :model do
 	end
 
 	describe 'validation' do
+		let!(:user) { build(:user) }
+
 		it { should validate_presence_of(:username) }
 		it { should validate_presence_of(:email) }
 		it { should validate_presence_of(:password) }
@@ -15,8 +17,6 @@ RSpec.describe User, type: :model do
 		it { should validate_length_of(:password).is_at_least(6) }
 
 		it 'should accept valid adresses' do
-			let!(:user) { build(:user) }
-
 			valid_addresess = %w[email@example.com firstname.lastname@example.com email@subdomain.example.com firstname+lastname@example.com email@123.123.123.123 email@[123.123.123.123] "email"@example.com 1234567890@example.com email@example-one.com _______@example.com email@example.name email@example.museum email@example.co.jp firstname-lastname@example.com]
 
 			valid_addresess.each do |adress|
@@ -26,8 +26,6 @@ RSpec.describe User, type: :model do
 		end
 
 		it 'shouldn not accept invalid adresses' do
-			let!(:user) { build(:user) }
-
 			invalid_adressess = ['plainaddress' '#@%^%#$@#$@#.com' '@example.com' 'Joe Smith' '<email@example.com>' 'mail.example.com' 'email@example@example.com' '”(),:;<>[\]@example.com' 'just”not”right@example.com']
 
 			invalid_adressess.each do |adress|
