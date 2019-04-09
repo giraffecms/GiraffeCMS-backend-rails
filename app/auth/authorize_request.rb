c67e6bc1beaa1fbc36b0e5742d38148b338a3a6f
kkm <kubasurdej@gmail.com>
2019-04-09 23:02:32 +0200

    Fix authorize_request file

--- app/auth/authorize_request.rb
+++ app/auth/authorize_request.rb
@@ -1,7 +1,17 @@
 class AuthorizeRequest
 	class << self
-		def call(token)
-			
+		def call(headers)
+			if auth_header
+				payload = JsonWebToken.decode(auth_header)
+				user_id = payload[:user_id]
+
+				User.find(user_id)
+			end
 		end
-	end
+
+		def auth_header(headers)
+			if headers['Authorization'].present?
+      	headers['Authorization'].split(' ').last
+      end
+    end
 end
