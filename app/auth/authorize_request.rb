55acc1fb0c55309f9d54ea73ccb434310b73de67
kkm <kubasurdej@gmail.com>
2019-04-10 16:58:30 +0200

    Request authorization spec, and fixes

--- app/auth/authorize_request.rb
+++ app/auth/authorize_request.rb
@@ -1,11 +1,11 @@
 class AuthorizeRequest
 	def self.call(headers)
-		if !headers.nil? && headers['Authorization'].present?
-			auth_header = headers['Authorization'].split(' ').last
-			payload = JsonWebToken.decode(auth_header)
-			user_id = payload[:user_id]
+		return false unless headers['Authorization'].present?
 
-			User.find(user_id)
-		end
+		auth_header = headers['Authorization'].split(' ').last
+		payload = JsonWebToken.decode(auth_header)
+		user_id = payload["user_id"]
+
+		User.find(user_id)
 	end
 end
