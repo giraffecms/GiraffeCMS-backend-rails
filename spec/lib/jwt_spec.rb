87bdcb8369946d5cf1b880c074119e9d62e9cf93
kkm <kubasurdej@gmail.com>
2019-04-10 16:27:10 +0200

    Add more test cases to JWT spec

--- spec/lib/jwt_spec.rb
+++ spec/lib/jwt_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 require_relative '../../lib/json_web_token.rb'
 
 RSpec.describe JsonWebToken do
-	let!(:token) { JsonWebToken.encode(payload) }
+	let(:token) { JsonWebToken.encode(payload) }
+	let(:decoded_token) { JsonWebToken.decode(token) }
+	let(:payload) { {user_id: user_id} }
+	let(:user_id) { 1 }
 
-	describe 'token encoding' do
-		let(:payload) { {user_id: 1} }
-		it 'encodes payload' do
-			expect(token).not_to be_nil
+	it 'encodes payload' do
+		expect(token).not_to be_nil
+	end
+
+	it 'decodes token' do
+		expect(decoded_token["user_id"]).to eq(user_id)
+	end
+
+	describe 'validation' do
+		let(:token) { JsonWebToken.encode(payload, 5.minutes.ago) }
+
+		it 'does not decode outdated token' do
+			expect{decoded_token}.to raise_error(JWT::ExpiredSignature)
 		end
 	end
 end
