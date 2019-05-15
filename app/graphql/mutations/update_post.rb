409e08c50b9fbe318a5cf435fb3745f690eee840
kkm <kubasurdej@gmail.com>
2019-05-15 19:23:10 +0200

    Mutation resolver, not passing tests

---
+++ app/graphql/mutations/update_post.rb
@@ -0,0 +1,42 @@
+module Mutations
+	class UpdatePost < Mutations::BaseMutation
+		argument :post, ID, required: true
+		argument :title, String, required: false
+		argument :body, String, required: false
+
+		field :post, Types::PostType, null: true
+		field :errors, [String], null: false
+
+		def resolve(post:, title: nil, body: nil) # body and title must be set to nil, because they arent required
+			if context[:current_user].try(:admin?)
+				update_post(post, title, body)
+			else
+				{
+					post: nil,
+					errors: ['You are not allowed to update posts']
+				}
+			end
+		end
+
+		private
+
+			def update_post(post_id, title, body)
+				post = Post.find(post_id)
+				attributes = {title: title, body: body}
+				post.update(attributes.delete_if { |k, v| v.nil? })
+
+				if post.save
+					context[:current_user].posts << post
+					{
+						post: post,
+						errors: [],
+					}
+				else
+					{
+						post: nil,
+						errors: post.errors.full_messages,
+					}
+				end
+			end
+	end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
   	field :create_post, mutation: Mutations::CreatePost
+  	field :update_post, mutation: Mutations::UpdatePost
   	field :create_user, mutation: Mutations::CreateUser
   	field :login, mutation: Mutations::Login
   	field :create_comment, mutation: Mutations::CreateComment
